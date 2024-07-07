08bcd80c377665001e93d86b095a041dd2ab888c
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-07-07 21:55:52 +0530

    Created fragments

--- app/src/main/java/com/example/cupid/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/cupid/ui/activity/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.cupid.ui.activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.example.cupid.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar?.hide()
-
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        navController=findNavController(R.id.navHostFragmentContainerView)
+        return  navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
