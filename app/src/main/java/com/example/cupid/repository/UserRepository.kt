69f0d91b074f473ff10290d27e8968fc7b9d198b
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-07-25 20:03:21 +0530

    Updated fragments

--- app/src/main/java/com/example/cupid/repository/UserRepository.kt
+++ app/src/main/java/com/example/cupid/repository/UserRepository.kt
@@ -1,5 +1,21 @@
 package com.example.cupid.repository
 
-class UserRepository {
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
 
+class UserRepository private val db = FirebaseFirestore.getInstance()
+
+suspend fun signUpUser(email: String, password: String): Result<String> {
+    return try {
+        val user = hashMapOf(
+            "email" to email,
+            "password" to password // For security reasons, ne
+        )
+        db.collection("users")
+            .add(user)
+            .await()
+        Result.success("Sign Up Successful")
+    } catch (e: Exception) {
+        Result.failure(e)
+    }
 }
