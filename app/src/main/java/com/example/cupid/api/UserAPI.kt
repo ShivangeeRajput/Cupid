74c466d50c882eb22254f53c1a6ed11256a4ff94
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-07-30 23:03:16 +0530

    Added auth API

--- app/src/main/java/com/example/cupid/api/UserAPI.kt
+++ app/src/main/java/com/example/cupid/api/UserAPI.kt
@@ -1,5 +1,16 @@
 package com.example.cupid.api
 
+import com.example.cupid.models.UserRequest
+import com.example.cupid.models.UserResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface UserAPI {
+    @POST("/auth/signup")
+    suspend fun signUp(@Body userRequest: UserRequest) : Response<UserResponse>
+
+    @POST("/auth/login")
+    suspend fun signIn(@Body userRequest: UserRequest) : Response<UserResponse>
 
 }
---
+++ app/src/main/java/com/example/cupid/di/NetworkModule.kt
@@ -0,0 +1,29 @@
+package com.example.cupid.di
+
+import com.example.cupid.api.UserAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+class NetworkModule {
+    @Singleton
+    @Provides
+    fun providesRetrofit(): Retrofit{
+       return Retrofit.Builder()
+           .addConverterFactory(GsonConverterFactory.create())
+           .baseUrl("")
+           .build()
+   }
+    @Singleton
+    @Provides
+    fun providesAuthAPI(retrofit: Retrofit): UserAPI {
+        return retrofit.create(UserAPI::class.java)
+    }
+}
