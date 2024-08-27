5e4177b8ccdc97757b441ad91bd40aff68463495
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-08-27 17:29:38 +0530

    Added DI

--- app/src/main/java/com/example/cupid/CupidApplicationClass.kt
+++ app/src/main/java/com/example/cupid/CupidApplicationClass.kt
@@ -1,8 +1,22 @@
 package com.example.cupid
 
 import android.app.Application
+import com.example.cupid.repository.UserRepository
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltAndroidApp
 class CupidApplicationClass : Application() {
+    @Inject
+    lateinit var userRepository: UserRepository
+    override fun onCreate() {
+        super.onCreate()
+        // Launching a coroutine here to call the suspend function
+       CoroutineScope(Dispatchers.IO).launch{
+           val result=userRepository.signUpUser("John@gmail.com","111111")
+       }
+    }
 }
--- app/src/main/java/com/example/cupid/repository/UserRepository.kt
+++ app/src/main/java/com/example/cupid/repository/UserRepository.kt
@@ -1,21 +1,26 @@
 package com.example.cupid.repository
 
+import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
 
-class UserRepository private val db = FirebaseFirestore.getInstance()
-
-suspend fun signUpUser(email: String, password: String): Result<String> {
-    return try {
-        val user = hashMapOf(
-            "email" to email,
-            "password" to password
-        )
-        db.collection("users")
-            .add(user)
-            .await()
-        Result.success("Sign Up Successful")
-    } catch (e: Exception) {
-        Result.failure(e)
+class UserRepository @Inject constructor(private val db: FirebaseFirestore) {
+    suspend fun signUpUser(email: String, password: String): Result<String> {
+        return try {
+            val user = hashMapOf(
+                "email" to email,
+                "password" to password
+            )
+            db.collection("users")
+                .add(user)
+                .await()
+            Result.success("Sign Up Successful")
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+    suspend fun saveUSer(email: String,password: String){
+        Log.d("TAG","User saved in database")
     }
 }
