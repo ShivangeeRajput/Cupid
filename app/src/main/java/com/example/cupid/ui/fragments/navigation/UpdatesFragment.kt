339d007fea90d274ea54db13839cdcf94246c274
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-08-28 13:27:16 +0530

    Added DI

---
+++ app/src/main/java/com/example/cupid/ui/fragments/navigation/UpdatesFragment.kt
@@ -0,0 +1,25 @@
+package com.example.cupid.ui.fragments.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cupid.R
+
+
+class UpdatesFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_updates, container, false)
+    }
+
+}
