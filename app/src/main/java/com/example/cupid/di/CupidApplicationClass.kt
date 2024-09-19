3b233dc96bb6b24929f2c2e70ba5fceb9a8adaa5
Shivangee Rajput <shivangeerajput185@gmail.com>
2024-09-19 09:37:20 +0530

    Added DI

--- app/src/main/java/com/example/cupid/di/CupidApplicationClass.kt
+++ app/src/main/java/com/example/cupid/di/CupidApplicationClass.kt
@@ -1,6 +1,7 @@
 package com.example.cupid.di
 
 import android.app.Application
+import android.util.Log
 import com.example.cupid.repository.UserRepository
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.CoroutineScope
@@ -11,6 +12,11 @@ import javax.inject.Inject
 @HiltAndroidApp
 class CupidApplicationClass : Application() {
       override fun onCreate() {
-        super.onCreate()
+          super.onCreate()
+          initLogging()
       }
+    private fun initLogging() {
+
+        Log.d("MyApp", "Logging initialized")
+    }
 }
